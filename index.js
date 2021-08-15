e9dbdf17b1273c7d138bb2fc2315e73a00a58f05
Mel Michael <mel.michael007@gmail.com>
2021-08-15 16:12:53 -0700

    Add mongo db connection

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 7777;
- 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+const DATABASE_NAME = 'mydb';
+
+const { MongoClient } = require('mongodb');
+const uri = 'mongodb://localhost:27017';
+
+MongoClient.connect(uri, function (err, client) {
+  if (err) throw err;
+  const database = client.db(DATABASE_NAME);
+  console.log(`You are now connected to ${DATABASE_NAME}`);
 });
- 
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
